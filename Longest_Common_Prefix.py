97093d77e66c0a7ed85a1fc743b6f2d63dc38bae
Tên của bạn <trongdoan7689@gmail.com>
2026-03-12 22:26:56 +0700

    12/3/2026

---
+++ Longest_Common_Prefix.py
@@ -0,0 +1,16 @@
+"""Hãy viết một hàm để tìm chuỗi tiền tố chung dài nhất trong một mảng các chuỗi.
+
+Nếu không có tiền tố chung, hãy trả về chuỗi rỗng ""."""
+
+
+
+class Solution:
+    def longestCommonPrefix(self, strs) -> str:
+        self.strs = strs
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    strs = ["flower","flow","flight"]
