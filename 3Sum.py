2c82554808f0c69b84aa6e4b44d5c8b28ce3c730
Tên của bạn <trongdoan7689@gmail.com>
2026-03-13 22:32:02 +0700

    13/3/2026

---
+++ 3Sum.py
@@ -0,0 +1,30 @@
+"""Cho một mảng số nguyên nums, hãy trả về tất cả các bộ ba [nums[i], nums[j], nums[k]]sao cho i != j, i != k, và j != k, và nums[i] + nums[j] + nums[k] == 0.
+
+Lưu ý rằng tập hợp lời giải không được chứa các bộ ba trùng lặp."""
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans=set()
+        nums.sort()
+        n=len(nums)
+        for i in range(n-2):
+            for j in range(i+1,n-1):
+                for k in range(j+1,n):
+                    temp=nums[i]+nums[j]+nums[k]
+                    if temp==0:
+                        ans.add((nums[i],nums[j],nums[k]))
+
+        lst = list(ans)
+        return lst
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    nums = [-100,-70,-60,110,120,130,160]
+
+    x = sol.threeSum(nums)
+    
+    print(x)
+
+    
--- Longest_Common_Prefix.py
+++ Longest_Common_Prefix.py
@@ -3,10 +3,17 @@
 Nếu không có tiền tố chung, hãy trả về chuỗi rỗng ""."""
 
 
-
 class Solution:
     def longestCommonPrefix(self, strs) -> str:
         self.strs = strs
+        pre = strs[0]
+        
+        for i in strs:
+            while not i.startswith(pre):
+                pre = pre[:-1]
+        
+        return pre         
+
 
 
 
@@ -14,3 +21,6 @@ if __name__ == "__main__":
     sol = Solution()
 
     strs = ["flower","flow","flight"]
+
+
+
