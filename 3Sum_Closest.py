b4300d61922db7cbb2f82564cf4b8531e0e69d48
Tên của bạn <trongdoan7689@gmail.com>
2026-03-15 22:34:24 +0700

    15/3/2026

--- 3Sum_Closest.py
+++ 3Sum_Closest.py
@@ -22,7 +22,28 @@ class Solution:
                         result = total
         
         return result
-
+# cách nhanh hơn 
+class Solution:
+    # @return an integer
+    def threeSumClosest(self, num, target):
+        num.sort()
+        result = num[0] + num[1] + num[2]
+        for i in range(len(num) - 2):
+            j, k = i+1, len(num) - 1
+            while j < k:
+                sum = num[i] + num[j] + num[k]
+                if sum == target:
+                    return sum
+                
+                if abs(sum - target) < abs(result - target):
+                    result = sum
+                
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+            
+        return result 
 if __name__ == "__main__":
     sol = Solution()
     nums = [-1,2,1,-4]
---
+++ Letter_Combinations_of_a_phone_number.py
@@ -0,0 +1,4 @@
+"""Cho một chuỗi chứa các chữ số từ 2-90 đến 1, hãy trả về tất cả các tổ hợp chữ cái có thể mà số đó có thể đại diện. Trả về kết quả theo bất kỳ thứ tự nào .
+
+Bảng đối chiếu các chữ số với các chữ cái (tương tự như trên các nút điện thoại) được đưa ra bên dưới. Lưu ý rằng số 1 không tương ứng với bất kỳ chữ cái nào."""
+
