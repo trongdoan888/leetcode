304bebda5bc63006c70ab361519d61d32f016fd5
Tên của bạn <trongdoan7689@gmail.com>
2026-03-17 21:51:21 +0700

    17/3/2026

---
+++ Valid_Parentheses.py
@@ -0,0 +1,7 @@
+"""Cho một chuỗi schỉ chứa các ký tự '(', ')', '{', '}', '['và ']', hãy xác định xem chuỗi đầu vào có hợp lệ hay không.
+
+Chuỗi đầu vào được coi là hợp lệ nếu:
+
+Dấu ngoặc mở phải được đóng bằng cùng loại dấu ngoặc.
+Các dấu ngoặc mở phải được đóng theo đúng thứ tự.
+Mỗi dấu ngoặc đóng đều có một dấu ngoặc mở tương ứng cùng loại."""
