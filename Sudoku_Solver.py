3547a2af09062cc6861de7046432ce33e7ddd47d
Tên của bạn <trongdoan7689@gmail.com>
2026-03-28 19:47:53 +0700

    28/3/2026

---
+++ Sudoku_Solver.py
@@ -0,0 +1,8 @@
+"""Hãy viết chương trình giải câu đố Sudoku bằng cách điền vào các ô trống.
+
+Lời giải Sudoku phải thỏa mãn tất cả các quy tắc sau :
+
+Mỗi chữ số 1-9phải xuất hiện chính xác một lần trong mỗi hàng.
+Mỗi chữ số 1-9phải xuất hiện chính xác một lần trong mỗi cột.
+Mỗi chữ số 1-9phải xuất hiện chính xác một lần trong mỗi ô nhỏ trong số 9 3x3ô của lưới.
+Ký tự này '.'biểu thị các ô trống."""
