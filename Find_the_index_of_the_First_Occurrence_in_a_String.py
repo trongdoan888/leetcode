c0bee104501147554426125bdee5fef68a1d40b3
Tên của bạn <trongdoan7689@gmail.com>
2026-04-02 22:52:39 +0700

    .

--- Find_the_index_of_the_First_Occurrence_in_a_String.py
+++ Find_the_index_of_the_First_Occurrence_in_a_String.py
@@ -1,1 +1,6 @@
 """Cho hai chuỗi needlevà haystack, hãy trả về chỉ số của lần xuất hiện đầu tiên của needletrong haystack, hoặc trả về -1nếu needlekhông phải là một phần của haystack."""
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+
+        return haystack.find(needle)
---
+++ Plus_One.py
@@ -0,0 +1,8 @@
+"""Bạn được cho một số nguyên lớn được biểu diễn dưới dạng một mảng số nguyên digits, trong đó mỗi phần tử digits[i]là chữ số của số nguyên đó. Các chữ số được sắp xếp từ chữ số quan trọng nhất đến chữ số ít quan trọng nhất theo thứ tự từ trái sang phải. Số nguyên lớn này không chứa bất kỳ chữ số đứng đầu nào.ith0
+
+Tăng số nguyên lớn lên một và trả về mảng các chữ số thu được ."""
+
+class Solution:
+    def plusOne(self, digits):
+        pass
+        
